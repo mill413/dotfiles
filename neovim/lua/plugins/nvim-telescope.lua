2aa5cd3780c189c3971da06ab4067cdd1c2df3f6
MillHaruto <mill413@outlook.com>
2024-12-22 22:22:33 +0800

    update nvim-telescope.lua in neovim

--- neovim/lua/plugins/nvim-telescope.lua
+++ neovim/lua/plugins/nvim-telescope.lua
@@ -1,5 +1,3 @@
-local builtin = require('telescope.builtin')
-
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
@@ -20,25 +18,32 @@ return {
             }
         }
     },
-    keys = {{
-        "<leader>ff",
-        builtin.find_files,
-        mode = 'n',
-        desc = "Telescope find files"
-    }, {
-        "<leader>fg",
-        builtin.live_grep,
-        mode = 'n',
-        desc = "Telescope live grep"
-    }, {
-        "<leader>fb",
-        builtin.buffers,
-        mode = 'n',
-        desc = "Telescope buffers"
-    }, {
-        "<leader>fh",
-        builtin.help_tags,
-        mode = 'n',
-        desc = "Telescope help tags"
-    }}
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    end,
+    -- keys = { {
+    --     "<leader>ff",
+    --     builtin.find_files,
+    --     mode = 'n',
+    --     desc = "Telescope find files"
+    -- }, {
+    --     "<leader>fg",
+    --     builtin.live_grep,
+    --     mode = 'n',
+    --     desc = "Telescope live grep"
+    -- }, {
+    --     "<leader>fb",
+    --     builtin.buffers,
+    --     mode = 'n',
+    --     desc = "Telescope buffers"
+    -- }, {
+    --     "<leader>fh",
+    --     builtin.help_tags,
+    --     mode = 'n',
+    --     desc = "Telescope help tags"
+    -- } }
 }
