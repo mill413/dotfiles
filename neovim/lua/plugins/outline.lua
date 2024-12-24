1f93bc6f0ebed95faf781725b2a1522d09897a61
MillHaruto <mill413@outlook.com>
2024-12-24 21:58:58 +0800

    add outline

--- neovim/lua/plugins/outline.lua
+++ neovim/lua/plugins/outline.lua
@@ -2,11 +2,25 @@ return {
     "hedyhli/outline.nvim",
     config = function()
         -- Example mapping to toggle outline
-        vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
-            { desc = "Toggle Outline" })
+        local function toggleOrFocus()
+            local outline = require("outline")
+            if outline.is_open() then
+                outline.focus_toggle()
+            else
+                outline.open({
+                    focus_outline = true
+                })
+            end
+        end
+        vim.keymap.set("n", "<leader>o", toggleOrFocus, {
+            desc = "Toggle Outline"
+        })
+        vim.keymap.set("n", "<leader>co", require("outline").close, {
+            desc = "Close Outline"
+        })
 
         require("outline").setup {
             -- Your setup opts here (leave empty to use defaults)
         }
-    end,
+    end
 }
