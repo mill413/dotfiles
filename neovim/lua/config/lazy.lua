808274e60a34e9b50d2d6022aa6ae089eb396500
MillHaruto <mill413@outlook.com>
2024-12-25 23:11:21 +0800

    add global keymap of opening lazy

--- neovim/lua/config/lazy.lua
+++ neovim/lua/config/lazy.lua
@@ -2,16 +2,19 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local out = vim.fn.system({"git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath})
     if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({ { "Failed to clone lazy.nvim:\n", "ErrorMsg" }, { out, "WarningMsg" },
-            { "\nPress any key to exit..." } }, true, {})
+        vim.api.nvim_echo({{"Failed to clone lazy.nvim:\n", "ErrorMsg"}, {out, "WarningMsg"},
+                           {"\nPress any key to exit..."}}, true, {})
         vim.fn.getchar()
         os.exit(1)
     end
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.keymap.set('n', "<leader>lp", "<cmd>Lazy<CR>", {
+    desc = "Open Lazy.nvim"
+})
 
 require("lazy").setup({
     spec = "plugins",
@@ -24,5 +27,5 @@ require("lazy").setup({
     checker = {
         enabled = true,
         notify = false
-    }
+    },
 })
