0e3780cd2cec5111d1f52c8379c5a0bc3d0ca92f
MillHaruto <mill413@outlook.com>
2024-12-25 23:08:24 +0800

    add toggleterm in edgy

--- neovim/lua/plugins/edgy.lua
+++ neovim/lua/plugins/edgy.lua
@@ -6,35 +6,47 @@ return {
         require("edgy").setup({
             animate = {
                 enabled = true, -- 是否启用动画
-                fps = 60,       -- 动画帧率
-                cps = 120,      -- 每秒字符数
+                fps = 60, -- 动画帧率
+                cps = 120 -- 每秒字符数
             },
-            right = {
-                {
-                    title = "File Explorer", -- 给 nvim-tree 起个名字
-                    ft = "NvimTree",         -- 文件类型
-                    size = { width = 42 },   -- 高度
-                    open = function()        -- 打开时调用的函数
-                        vim.cmd("NvimTreeOpen")
-                    end,
-                },
-                {
-                    title = "Outline",     -- 给 outline.nvim 起个名字
-                    ft = "Outline",        -- 文件类型
-                    size = { width = 42 }, -- 高度
-                    open = function()      -- 打开时调用的函数
-                        vim.cmd("OutlineOpen")
-                    end,
-                },
-            },
+            right = {{
+                title = "File Explorer", -- 给 nvim-tree 起个名字
+                ft = "NvimTree", -- 文件类型
+                size = {
+                    width = 42
+                }, -- 高度
+                open = function() -- 打开时调用的函数
+                    vim.cmd("NvimTreeOpen")
+                end
+            }, {
+                title = "Outline", -- 给 outline.nvim 起个名字
+                ft = "Outline", -- 文件类型
+                size = {
+                    width = 42
+                }, -- 高度
+                open = function() -- 打开时调用的函数
+                    vim.cmd("OutlineOpen")
+                end
+            }},
+            bottom = { -- 将 toggleterm 添加到底部
+            {
+                title = "Terminal", -- 窗口标题
+                ft = "toggleterm", -- 匹配 filetype
+                size = {
+                    height = 25
+                }, -- 默认高度
+                open = function()
+                    vim.cmd("ToggleTerm direction=horizontal")
+                end
+            }},
             options = {
                 right = {
-                    winfixwidth = true,
-                },
+                    winfixwidth = true
+                }
                 -- bottom = {
                 --     winfixheight = true,
                 -- },
-            },
+            }
         })
     end
 }
