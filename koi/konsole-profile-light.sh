0ad358977e210d1f2229989a8c7099b3d5f1b5a4
MillHaruto <mill413@outlook.com>
2025-02-23 21:54:50 +0800

    add konsole profile of koi custom scripts

---
+++ koi/konsole-profile-light.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+PROFILE="Light"
+
+for instance in $(qdbus6 | grep org.kde.konsole); do
+    for session in $(qdbus6 "$instance" | grep -E '^/Sessions/'); do
+        qdbus6 "$instance" "$session" org.kde.konsole.Session.setProfile "$PROFILE"
+    done
+done
+
+kwriteconfig6 --file ~/.config/konsolerc --group "Desktop Entry" --key DefaultProfile "$PROFILE.profile"
+# konsoleprofile "colors=KlassyLight"
